deaf529a049f61ddd4b37b2ebd34fb1faa05e67a
jakhongirs <j.sagdullaev31@gmail.com>
2022-08-09 22:43:12 +0500

    feat: student, unicersity and studentsponsor models

--- metsenat/models.py
+++ metsenat/models.py
@@ -29,11 +29,22 @@ SPONSOR_STATUS_CHOICES = (
 UZCARD = "uzcard"
 HUMO = "humo"
 VISA = "visa"
+CASH = 'cash'
 
 PAYMENT_TYPE_CHOICES = (
     (UZCARD, "uzcard"),
     (HUMO, "humo"),
     (VISA, "visa"),
+    (CASH, "cash"),
+)
+
+# STUDENT TYPE CHOICES:
+BACHELOR = "bakalavr"
+MASTERS = "magistr"
+
+STUDENT_TYPE_CHOICES = (
+    (BACHELOR, "bakalavr"),
+    (MASTERS, "magistr")
 )
 
 
@@ -49,7 +60,35 @@ class Sponsor(BaseModel):
     register_datetime = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=100, choices=SPONSOR_STATUS_CHOICES, default=SPONSOR_STATUS_CHOICES[0][0])
     spent_amount = models.DecimalField(max_digits=19, decimal_places=2, default=0)
-    payment_type = models.CharField(max_length=100, choices=PAYMENT_TYPE_CHOICES, null=True, blank=True)
+    payment_type = models.CharField(max_length=100, choices=PAYMENT_TYPE_CHOICES, default=SPONSOR_STATUS_CHOICES[3][0])
 
     def __str__(self):
         return self.name
+
+
+# UNIVERSITY:
+class University(BaseModel):
+    name = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# STUDENT:
+class Student(BaseModel):
+    name = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128, unique=True, null=True, blank=True)
+    student_type = models.CharField(max_length=100, choices=STUDENT_TYPE_CHOICES)
+    received_amount = models.DecimalField(max_digits=19, decimal_places=2, default=0)
+    tuition_fee = models.DecimalField(max_digits=19, decimal_places=2, default=0)
+    phone_number = models.CharField(max_length=128)
+
+    university = models.ForeignKey(University, on_delete=models.CASCADE, related_name='students')
+
+
+# STUDENT SPONSOR:
+class StudentSponsor(BaseModel):
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name='students')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='sponsors')
+    amount = models.DecimalField(max_digits=19, decimal_places=2, default=0)
